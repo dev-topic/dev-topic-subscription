c032d3fc9e7b591f9cd0ca6c81e378c2f75e4940
ahn-sj <jaesa5221@gmail.com>
2024-03-03 15:29:13 +0900

    Initial Project

---
+++ src/main/java/com/tally/topicsubscription/TopicSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.tally.topicsubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicSubscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopicSubscriptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tally/topicsubscription/TopicSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tally.topicsubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicSubscriptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
